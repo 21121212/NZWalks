3f24072ea9308cc4f27f45bf7dae6eaac9fa60c3
Lodwin.Moloto@media24.com <Lodwin.Moloto@media24.com>
2022-10-17 10:24:49 +0200

    End of Section 2

---
+++ NZWalks/NZWalksd.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksd.API.Models.Domain;
+
+namespace NZWalksd.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options)
+        {
+
+        }
+        
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalksDifficulty { get; set; }
+    }
+}
---
+++ NZWalks/NZWalksd.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,11 @@
+﻿namespace NZWalksd.API.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+
+        //Navigattion Property
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
